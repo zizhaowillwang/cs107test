e936513c91fd203bf4a347aef7c106153f2aaf96
Zizhao Will Wang <willwang@Zizhaos-MacBook-Pro.local>
2020-10-27 20:38:52 -0400

    new

---
+++ test_roots.py
@@ -0,0 +1,13 @@
+import pytest
+import roots
+
+def test_quadroots_result():
+    assert roots.quad_roots(1.0, 1.0, -12.0) == ((3+0j), (-4+0j))
+
+def test_quadroots_types():
+    with pytest.raises(TypeError):
+        roots.quad_roots("", "green", "hi")
+
+def test_quadroots_zerocoeff():
+    with pytest.raises(ValueError):
+        roots.quad_roots(a=0.0)
